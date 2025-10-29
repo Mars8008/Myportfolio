b05f9188ddf3b8217fcae9ec527a28bcda4a3b48
Angarag Ulziibadrakh <angaragulziibadrakh@dyn-10-140-168-202.d.umn.edu>
2025-10-28 20:36:04 -0500

    darkmode update

--- components/sections/Static2.tsx
+++ components/sections/Static2.tsx
@@ -27,7 +27,7 @@ export default function Static2() {
 										<div className="content mx-auto">
 											<i className="ri-computer-line text-primary-2" />
 											<h2 className="text-300 my-0 fs-50">
-												<CountUp className="odometer text-dark fw-medium" enableScrollSpy={true} end={20} />
+												<CountUp className="odometer text-dark fw-medium" enableScrollSpy={true} end={15} />
 												<span className="fs-50 text-300 mb-0">+</span>
 											</h2>
 											<p className="fs-6 mb-0 text-dark">Projects Completed</p>
@@ -39,7 +39,7 @@ export default function Static2() {
 										<div className="content mx-auto">
 											<i className="ri-service-line text-primary-2" />
 											<h2 className="text-300 my-0 fs-50">
-												<CountUp className="odometer text-dark fw-medium" enableScrollSpy={true} end={20} />
+												<CountUp className="odometer text-dark fw-medium" enableScrollSpy={true} end={15} />
 												<span className="fs-50 text-300 mb-0">+</span>
 											</h2>
 											<p className="fs-6 mb-0 text-dark">Satisfied Clients</p>
